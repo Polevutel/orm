fa7e9ba25ac6eb1fa00ef4617aa6350cd5f1ffd6
Vlad <vlad-vlad315220@mail.ru>
2024-03-09 14:15:56 +0300

    commit

--- django_netology/app/models.py
+++ django_netology/app/models.py
@@ -3,36 +3,11 @@ from django.db import models
 class Phone(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='images/')
+    image = models.URLField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    release_date = models.DateField()
-    lte_exists = models.BooleanField()
-    slug = models.SlugField(max_length=100)
-import csv
-from datetime import datetime
-from yourapp.models import Phone
-from django.utils.text import slugify
+    releasedate = models.DateField()
+    lteexists = models.BooleanField()
+    slug = models.SlugField(unique=True)
 
-def import_data_from_csv(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        csv_data = csv.DictReader(file, delimiter=';')
-        for row in csv_data:
-            phone = Phone(
-                id=row['id'],
-                name=row['name'],
-                image=row['image'],
-                price=row['price'],
-                release_date=datetime.strptime(row['release_date'], '%Y-%m-%d').date(),
-                lte_exists=row['lte_exists'] == 'True',
-                slug=slugify(row['name'])
-            )
-            phone.save()
-def get_sorted_phones(request):
-    sort_param = request.GET.get('sort', '')
-    if sort_param == 'name':
-        phones = Phone.objects.all().order_by('name')
-    elif sort_param == 'min_price':
-        phones = Phone.objects.all().order_by('price')
-    else:
-        phones = Phone.objects.all()
-    return phones
+    def __str__(self):
+        return self.name
--- django_netology/app/views.py
+++ django_netology/app/views.py
@@ -1,3 +1,33 @@
+from datetime import datetime
+import csv
+from yourapp.models import Phone
+from django.utils.text import slugify
 from django.shortcuts import render
 
-# Create your views here.
+
+def importdatafromcsv(filepath):
+    with open(filepath, 'r', encoding='utf-8') as file:
+        csvdata = csv.DictReader(file, delimiter=';')
+        for row in csvdata:
+            phone = Phone(
+                id=row['id'],
+                name=row['name'],
+                image=row['image'],
+                price=row['price'],
+                releasedate=datetime.strptime(row['releasedate'], '%Y-%m-%d').date(),
+                lteexists=row['lteexists'] == 'True',
+                slug=slugify(row['name'])
+            )
+            phone.save()
+
+
+def getsortedphones(request):
+    sortparam = request.GET.get('sort', '')
+    if sortparam == 'name':
+        phones = Phone.objects.all().order_by('name')
+    elif sortparam == 'minprice':
+        phones = Phone.objects.all().order_by('price')
+    else:
+        phones = Phone.objects.all()
+
+    return render(request, 'phones_list.html', {'phones': phones})
